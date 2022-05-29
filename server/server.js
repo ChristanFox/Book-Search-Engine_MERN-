b4f741c14c206eab800315e5b8e9856baf1b1b1b
christanfox <bayshorestinks@aol.com>
2022-05-29 09:52:14 -0400

    updated authorization and mutations files

--- server/server.js
+++ server/server.js
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
